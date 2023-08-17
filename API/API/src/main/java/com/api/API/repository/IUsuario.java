123e7e5d34d53b8e433e73658d19b733a4a48f11
Luis Gustavo Soares <luissoaresg09@gmail.com>
2023-08-17 16:28:18 -0300

    index.html
    Interface code HTML

--- API/API/src/main/java/com/api/API/repository/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.api.API.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.API.model.Usuario;
-
-public interface IUsuario extends JpaRepository<Usuario, Integer>{
-    
-}
